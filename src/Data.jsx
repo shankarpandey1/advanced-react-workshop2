2b72f166069491d97d45f425f887c3ddcf142589
Jorge Macana <39782680+andresmacana@users.noreply.github.com>
2023-06-27 21:20:18 -0400

    WS2 2

--- src/Data.jsx
+++ src/Data.jsx
@@ -1,21 +1,43 @@
-const bizcards = [
-  { name: "John Doe", position: "Contractor", email: null },
+const businessCards = [
   {
-    name: "Zoe Doe",
-    position: "Contractor",
-    email: null,
-    phone: null,
-    photo: "<<url-to-the-photo>>",
-  },
-  {
-    name: "Edgar Poe",
-    position: "CEO",
-    email: "edgar.poe@example.com",
-    phone: "+1 (123) 456-7890",
+    id: 1,
+    name: "Jorge",
+    email: "jorge@example.com",
+    tel: "111-1111",
     photo:
       "https://banffventureforum.com/wp-content/uploads/2019/08/no-photo-icon-22.png",
   },
-  // ...
+  {
+    id: 2,
+    name: "Shankar",
+    email: "shankar@example.com",
+    tel: "222-2222",
+    photo:
+      "https://banffventureforum.com/wp-content/uploads/2019/08/no-photo-icon-22.png",
+  },
+  {
+    id: 3,
+    name: "Lucian",
+    email: "lucian@example.com",
+    tel: "333-3333",
+    photo:
+      "https://banffventureforum.com/wp-content/uploads/2019/08/no-photo-icon-22.png",
+  },
+  {
+    id: 4,
+    name: "Ali",
+    email: "ali@example.com",
+    tel: "444-4444",
+    photo:
+      "https://banffventureforum.com/wp-content/uploads/2019/08/no-photo-icon-22.png",
+  },
+  {
+    id: 5,
+    name: "Jason",
+    email: "jason@example.com",
+    tel: "555-5555",
+    photo:
+      "https://banffventureforum.com/wp-content/uploads/2019/08/no-photo-icon-22.png",
+  },
 ];
-
-export default bizcards;
+export default businessCards;
