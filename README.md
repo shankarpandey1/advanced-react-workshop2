f9eb85c14895f75073759c0d7317aee104e69e17
Jorge Macana <39782680+andresmacana@users.noreply.github.com>
2023-06-28 08:29:41 -0400

    3

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Workshop 2
+
+This application covers **module 3 and 4**.
+The **App.js** file covers module 4 through challenge 2.
+The **AppJump.jsx** file covers module 4 through challenge 3.
+For practical purposes, we will continue with the App.js file for module 5.
--- src/App.js
+++ src/App.js
@@ -8,43 +8,6 @@ function App() {
     <div className="App">
       <h2>Business Cards</h2>
       <BusinessCardList bizcards={bizcards} />
-      {/* <div className="cards">
-        <BusinessCard
-          name="John Doe"
-          position="CEO"
-          email="john.doe@example.com"
-          tel="+1 (123) 456-7890"
-          photo="https://banffventureforum.com/wp-content/uploads/2019/08/no-photo-icon-22.png"
-        />
-        <BusinessCard
-          name="Jane Doe"
-          position="CTO"
-          email="jane.doe@example.com"
-          tel="+1 (123) 456-7890"
-          photo="https://banffventureforum.com/wp-content/uploads/2019/08/no-photo-icon-22.png"
-        />
-        <BusinessCard
-          name="Bob Smith"
-          position="CFO"
-          email="bob.smith@example.com"
-          tel="+1 (123) 456-7890"
-          photo="https://banffventureforum.com/wp-content/uploads/2019/08/no-photo-icon-22.png"
-        />
-        <BusinessCard
-          name="Alice Johnson"
-          position="COO"
-          email="alice.johnson@example.com"
-          tel="+1 (123) 456-7890"
-          photo="https://banffventureforum.com/wp-content/uploads/2019/08/no-photo-icon-22.png"
-        />
-        <BusinessCard
-          name="Charlie Brown"
-          position="CMO"
-          email="charlie.brown@example.com"
-          tel="+1 (123) 456-7890"
-          photo="https://banffventureforum.com/wp-content/uploads/2019/08/no-photo-icon-22.png"
-        />
-      </div> */}
     </div>
   );
 }
